4db6c853b4522ecf50880d25731c30b5db1ed374
Asmaa Hamid <asmaahamid002@gmail.com>
2024-10-03 15:04:06 +0300

    removed session attribute

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,17 +1,10 @@
 'use client'
 
-import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 
-const AuthProvider = ({
-  children,
-  session,
-}: {
-  children: React.ReactNode
-  session: Session
-}) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>
 }
 
 export default AuthProvider
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import Header from '@/components/Header'
 import AuthProvider from './AuthProvider'
 import { Toaster } from 'react-hot-toast'
-import { Session } from 'next-auth'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -19,17 +18,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode
-  session: Session
 }>) {
   return (
     <html lang='en'>
       <body
         className={`${poppins.variable} antialiased min-h-screen w-full flex flex-col bg-gray-50 text-gray-800`}
       >
-        <AuthProvider session={session}>
+        <AuthProvider>
           <Header />
           <main className='flex-1 w-full flex p-6'>
             <div className='w-full max-w-3xl mx-auto flex flex-col justify-center items-center bg-white shadow-lg rounded-lg p-4'>
